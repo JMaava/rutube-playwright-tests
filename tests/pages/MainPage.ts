ed9e8173e1bb956f65ebe2c4a1e2a860591b2b86
Volkova Maria <MaRi221994@yandex.ru>
2025-08-17 19:29:16 +0700

    add mainPage tests

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -1,8 +1,31 @@
-import { Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class MainPage extends BasePage {
+  private readonly headerLocator: Locator;
+  private readonly categoriesTabsLocator: Locator;
+  private readonly menuLocator: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.headerLocator = page.locator('.header-module__charHeaderWrapper');
+    this.categoriesTabsLocator = page.locator('.wdp-tabs-module__tabs');
+    this.menuLocator = page.getByLabel('Облегченная панель навигации');
+  }
+
   async open() {
     this.page.goto('https://rutube.ru/');
+  }
+
+  async headerHasCorrectAriaSnapshot() {
+    await expect(this.headerLocator).toMatchAriaSnapshot();
+  }
+
+  async categoriesTabsHasCorrectAriaSnapshot() {
+    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot();
+  }
+
+  async menuHasCorrectAriaSnapshot() {
+    await expect(this.menuLocator).toMatchAriaSnapshot();
   }
 }
