0f9f78db9a3bf57ad9003cd1080de5dbfd021169
Volkova Maria <MaRi221994@yandex.ru>
2025-08-19 18:19:18 +0700

    add tests forCreatorsPage

---
+++ tests/pages/ForCreatorsPage.ts
@@ -0,0 +1,56 @@
+import { Locator, Page } from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class ForCreatorsPage extends BasePage {
+  static readonly testsParam = [
+    {
+      url: 'https://rutube.ru/for_creators/#main',
+      screenshotName: 'mainTab.png',
+      name: 'Главная',
+    },
+    {
+      url: 'https://rutube.ru/for_creators/#steps',
+      screenshotName: 'stepsTab.png',
+      name: 'Первые шаги',
+    },
+    {
+      url: 'https://rutube.ru/for_creators/#faq',
+      screenshotName: 'faqTab.png',
+      name: 'Как развивать канал',
+    },
+    {
+      url: 'https://rutube.ru/for_creators/#monetization',
+      screenshotName: 'monetizationTab.png',
+      name: 'Монетизация',
+    },
+    {
+      url: 'https://rutube.ru/for_creators/#rules',
+      screenshotName: 'rulesTab.png',
+      name: 'Правила',
+    },
+    {
+      url: 'https://rutube.ru/for_creators/#team',
+      screenshotName: 'teamTab.png',
+      name: 'Команда R',
+    },
+  ];
+
+  private readonly pageContentLocator: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.pageContentLocator = this.page.locator('#___gatsby');
+  }
+
+  // actions
+
+  async open(url: string) {
+    await this.page.goto(url);
+  }
+
+  // assertions
+
+  async pageHasCorrectLayout(screenshotName: string) {
+    await this.checkLayoutByScreenshot(this.pageContentLocator, screenshotName);
+  }
+}
